6f9c9b0edda199a3587f1b2707415675d7fafb5b
Shaun Curtis <shaun@coldelm.com>
2021-02-22 22:04:57 +0000

    Evining Commit

--- CEC.Blazor.Examples/Client/Program.cs
+++ CEC.Blazor.Examples/Client/Program.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using CEC.Blazor.Examples.Components;
 using CEC.Blazor.Examples.Services;
 
 namespace CEC.Blazor.Examples.Client
@@ -17,7 +12,7 @@ namespace CEC.Blazor.Examples.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<CEC.Blazor.Examples.Components.WASM>("app");
+            builder.RootComponents.Add<CEC.Blazor.Examples.Components.WASM>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IWeatherForecastService, WeatherForecastWASMService>();
